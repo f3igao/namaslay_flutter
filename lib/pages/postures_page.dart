43d19dee3b6a7228b827d492ff9e6bd3c82225bf
f3igao <f3igao@icloud.com>
2019-06-02 02:48:09 -0400

    refactor posture

--- lib/pages/postures_page.dart
+++ lib/pages/postures_page.dart
@@ -50,22 +50,6 @@ class PosturesPage extends StatelessWidget {
   }
 }
 
-// class Filter {
-//   const Filter({this.title});
-//   final String title;
-// }
-
-// const List<Filter> filters = const <Filter>[
-//   const Filter(title: 'All'),
-//   const Filter(title: 'Recommended'),
-//   const Filter(title: 'Lower Back'),
-//   const Filter(title: 'Arms'),
-//   const Filter(title: 'Legs'),
-//   const Filter(title: 'Core'),
-//   const Filter(title: 'Shoulders'),
-//   const Filter(title: 'Neck'),
-// ];
-
 class PosturesContainer extends StatelessWidget {
   const PosturesContainer({Key key, this.filter}) : super(key: key);
   final Filter filter;
@@ -99,74 +83,6 @@ class PosturesContainer extends StatelessWidget {
               ),
             ),
           ],
-        )
-        );
+        ));
   }
 }
-
-// class PosturesContainer extends StatelessWidget {
-//   const PosturesContainer({Key key, this.filter}) : super(key: key);
-//   final Filter filter;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: EdgeInsets.all(8.0),
-//       color: Colors.white,
-//       child: Column(
-//         children: <Widget>[
-//           Expanded(
-//               child: ListView.builder(
-//             itemBuilder: _buildBody,
-//             itemCount: mockPostures.length,
-//           ))
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// Widget _buildBody(BuildContext context, int index) {
-//  return StreamBuilder<QuerySnapshot>(
-//    stream: Firestore.instance.collection('poses').snapshots(),
-//    builder: (context, snapshot) {
-//      if (!snapshot.hasData) return LinearProgressIndicator();
-//      return _buildPosturesRow(context, snapshot.data.documents);
-//    },
-//  );
-// }
-
-// Widget _buildPosturesRow(BuildContext context, List<DocumentSnapshot> poses) {
-//   return Row(
-//     children: poses.map((pose) => _buildPostureTile(context, pose)).toList(),
-//     // children: [
-//     // Expanded(
-//     //     child: _buildPostureTile(context, index)),
-//     // Expanded(
-//     //   child: Container(
-//     //     child: Center(child: Text('test')),
-//     //     // child: Center(child: Text('test')),
-//     //     height: 170.0,
-//     //     margin: EdgeInsets.all(8.0),
-//     //     decoration: new BoxDecoration(
-//     //       color: Colors.amber,
-//     //       borderRadius: new BorderRadius.all(new Radius.circular(16.0)),
-//     //     ),
-//     //   ),
-//     // )
-//   // ]);
-//   );
-// }
-
-// Widget _buildPostureTile(BuildContext context, DocumentSnapshot pose) {
-// // final record = Record.fromSnapshot(pose);
-// final TextStyle textStyle = Theme.of(context).textTheme.display1;
-//   return Container(
-//       child: Center(child: Text(pose.data['name'], style: textStyle)),
-//       height: 170.0,
-//       margin: EdgeInsets.all(8.0),
-//       decoration: new BoxDecoration(
-//         color: Colors.amber,
-//         borderRadius: new BorderRadius.all(new Radius.circular(16.0)),
-//       ),
-//     );
-// }
