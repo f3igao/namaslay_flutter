23bac29e54fd0208c964c98d6f07d339bcd9f9ae
Fei <f3i.g4o@gmail.com>
2019-07-21 19:47:45 -0400

    add subscription

--- lib/dialogs/subscription_dialog.dart
+++ lib/dialogs/subscription_dialog.dart
@@ -1,26 +1,23 @@
-//import 'dart:async';
-//
-//import 'package:flutter/material.dart';
-//import 'package:namaslay_flutter/widgets/cached_image.dart';
-//import 'package:namaslay_flutter/widgets/countdown.dart';
-//
-//class SubscriptionDialog extends StatefulWidget {
-//  @override
-//  SubscriptionDialogState createState() => SubscriptionDialogState();
-//}
-//
-//class SubscriptionDialogState extends State<SubscriptionDialog> {
-//  InAppPurchaseConnection _iap = InAppPurchaseConnection.instance;
-//
-//  @override
-//  void initState() {
-////    _initialize();
-//    super.initState();
-//  }
-//
-//  @override
-//  void dispose() {
-////    _subscription.cancel();
-//    super.dispose();
-//  }
-//}
+import 'package:flutter/material.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+
+class SubscriptionDialog extends StatefulWidget {
+  @override
+  SubscriptionDialogState createState() => SubscriptionDialogState();
+}
+
+class SubscriptionDialogState extends State<SubscriptionDialog> {
+  InAppPurchaseConnection _iap = InAppPurchaseConnection.instance;
+
+  @override
+  void initState() {
+//    _initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+//    _subscription.cancel();
+    super.dispose();
+  }
+}
