3d031f6f035c9492caff1499efbaaf484852b6f2
Fei <f3i.g4o@gmail.com>
2019-07-21 20:01:01 -0400

    wip: sub

--- lib/dialogs/subscription_dialog.dart
+++ lib/dialogs/subscription_dialog.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
+// import 'package:flutter/material.dart';
+// import 'package:in_app_purchase/in_app_purchase.dart';
 
-class SubscriptionDialog extends StatefulWidget {
-  @override
-  SubscriptionDialogState createState() => SubscriptionDialogState();
-}
+// class SubscriptionDialog extends StatefulWidget {
+//   @override
+//   SubscriptionDialogState createState() => SubscriptionDialogState();
+// }
 
-class SubscriptionDialogState extends State<SubscriptionDialog> {
-  InAppPurchaseConnection _iap = InAppPurchaseConnection.instance;
+// class SubscriptionDialogState extends State<SubscriptionDialog> {
+//   InAppPurchaseConnection _iap = InAppPurchaseConnection.instance;
 
-  @override
-  void initState() {
-//    _initialize();
-    super.initState();
-  }
+//   @override
+//   void initState() {
+// //    _initialize();
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-//    _subscription.cancel();
-    super.dispose();
-  }
-}
+//   @override
+//   void dispose() {
+// //    _subscription.cancel();
+//     super.dispose();
+//   }
+// }
